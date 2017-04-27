4a6faf1582f7cf8c89cb2ce091a69e74f8f4ec9b
asternaut <snooks.sheild@gmail.com>
2017-04-26 19:14:58 -0600

    where'd these changes even come from?!

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ A schema is like a blueprint, and it defines what all future heroes will look li
 
 `app.set('view engine', 'ejs');`
 `app.set('views', path.join(__dirname, 'views'));`
+
+Everything in "client" is a react app, which is separate from the rest of our code. To start the react code base, cd into 'client' and running `nmp start`.
+
+Everything outside of client is our node-specific app - it only manages our DB and API endpoints. We can startup this project with `nodemon` or `node server`.
+
+Each app has it own node-modules and package.json.
+
+We're going to blend the start-up of both apps through a command. To start both simultaneously, use `npm run dev`. --This command was created in our node package.json.
--- server.js
+++ server.js
@@ -12,30 +12,23 @@ var mongoose = require('mongoose');
 // it will look for/create a db called superheros
 mongoose.connect("mongodb://localhost/superheroes");
 
-app.use(express.static('public'));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-app.get('/', function(req,res){
-  res.render('index')
-});
-
-app.get('/superheroes', function(req,res){
-  res.render('superheroes')
-});
-
-app.get('/villains', function(req,res){
-  res.render('villains')
-});
-
 app.use('/api/superheroes', heroRoutes); // type this once by putting it here
 
-var server  = app.listen(3000, function(){
-  console.log('Server tuned into PORT 3000');
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/public/index.html'));
+});
+
+var server  = app.listen(3001, function(){
+  console.log('Server tuned into PORT 3001');
 });
 
 module.exports = app;
